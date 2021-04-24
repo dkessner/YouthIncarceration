f8fa2549210133ba99761fc3e03ae983f9688498
Darren Kessner <Darren.Kessner@marlborough.org>
2021-04-24 12:51:57 -0700

    added csv reading; added area numbers to division info

--- interactive_map.js
+++ interactive_map.js
@@ -1,42 +1,49 @@
-let img;
+let lapd_division_map;
+let lhda;
 
 let label_positions = [
-    {name: "Devonshire", x:120, y:80},
-    {name: "Foothill", x:277, y:88},
-    {name: "West Valley", x:82, y:171},
-    {name: "Van Nuys", x:186, y:167},
-    {name: "North Hollywood", x:257, y:167},
-    {name: "West Los Angeles", x:149, y:277},
-    {name: "Hollywood", x:306, y:264},
-    {name: "Northeast", x:405, y:265},
-    {name: "Wilshire", x:306, y:318},
-    {name: "Rampart", x:367, y:301},
-    {name: "Central", x:398, y:320},
-    {name: "Hollenbeck", x:452, y:298},
-    {name: "Southwest", x:321, y:357},
-    {name: "Newton", x:384, y:370},
-    {name: "77th St", x:336, y:403},
-    {name: "Pacific", x:240, y:431},
-    {name: "Southeast", x:380, y:451},
-    {name: "Harbor", x:368, y:632},
+    {area: 1, name: "Central", x:398, y:320},
+    {area: 2, name: "Rampart", x:367, y:301},
+    {area: 3, name: "Southwest", x:321, y:357},
+    {area: 4, name: "Hollenbeck", x:452, y:298},
+    {area: 5, name: "Harbor", x:368, y:632},
+    {area: 6, name: "Hollywood", x:306, y:264},
+    {area: 7, name: "Wilshire", x:306, y:318},
+    {area: 8, name: "West Los Angeles", x:149, y:277},
+    {area: 9, name: "Van Nuys", x:186, y:167},
+    {area: 10, name: "West Valley", x:82, y:171},
+    {area: 11, name: "Northeast", x:405, y:265},
+    {area: 12, name: "77th St", x:336, y:403},
+    {area: 13, name: "Newton", x:384, y:370},
+    {area: 14, name: "Pacific", x:240, y:431},
+    {area: 15, name: "North Hollywood", x:257, y:167},
+    {area: 16, name: "Foothill", x:277, y:88},
+    {area: 17, name: "Devonshire", x:120, y:80},
+    {area: 18, name: "Southeast", x:380, y:451},
+    {area: 19, name: "Mission", x:0, y:0},
+    {area: 20, name: "Olympic", x:0, y:0},
+    {area: 21, name: "Topanga", x:0, y:0}
 ];
 
 function preload() {
-    img = loadImage("lapd_division_map.jpg");
+    lapd_division_map = loadImage("lapd_division_map.jpg");
+    lhda = loadTable("dfFinalCombined.csv", "csv", "header");
 }
 
 function setup() {
     createCanvas(480, 718);
+    console.log(lhda.getRowCount());
 }
 
 function draw() {
     background(100);
-    image(img, 0, 0);
+    image(lapd_division_map, 0, 0);
 
     let selected = "";
+
     for (let label of label_positions) {
         if (dist(mouseX, mouseY, label.x, label.y) < 20)
-            selected = label.name;
+            selected = label.area + " " + label.name;
     }
 
     text(selected, 50, height-100);
